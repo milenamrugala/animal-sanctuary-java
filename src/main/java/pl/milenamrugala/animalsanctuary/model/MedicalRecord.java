42642f0155aaba46893ded96b5337754a0b1b13e
milenamrugala <milena.mrugala@icloud.com>
2024-08-15 20:03:58 +0200

    MedicalRecord class added

---
+++ src/main/java/pl/milenamrugala/animalsanctuary/model/MedicalRecord.java
@@ -0,0 +1,38 @@
+package pl.milenamrugala.animalsanctuary.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@ToString
+
+@Entity
+public class MedicalRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    private String treatment;
+
+    @Column(name = "is_in_treatment")
+    private boolean isInTreatment;
+
+    @ManyToOne
+    @JoinColumn(name = "animal_id")
+    private Animal animal;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+}
