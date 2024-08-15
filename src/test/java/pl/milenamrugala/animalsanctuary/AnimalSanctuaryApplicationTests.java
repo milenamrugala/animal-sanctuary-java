41fd6ed0a12948d853befc01cdd28a24dcb52855
milenamrugala <milena.mrugala@icloud.com>
2024-08-15 20:04:40 +0200

    Initial commit

---
+++ src/test/java/pl/milenamrugala/animalsanctuary/AnimalSanctuaryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.milenamrugala.animalsanctuary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalSanctuaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
