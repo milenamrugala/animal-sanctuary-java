1beb4885185fa278e7d16b6c31523aabf4ac765d
milenamrugala <milena.mrugala@icloud.com>
2024-08-18 00:50:22 +0200

    Comments explaining each line added.

--- src/main/java/pl/milenamrugala/animalsanctuary/controller/HomeController.java
+++ src/main/java/pl/milenamrugala/animalsanctuary/controller/HomeController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+@Controller // marks the class as a Spring MVC controller, which will handle HTTP requests and return responses
 public class HomeController {
 
-    @RequestMapping("/homepage")
-    @ResponseBody
+    @RequestMapping("/homepage") // used to map HTTP requests to handler methods in the controller
+    @ResponseBody // indicates that the return value of the method should be bound to the web response body
     public String home() {
         return "Hello World!";
     }
